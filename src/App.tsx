9da8a06c2b156fc328f2a884aa206ac5b56d64ba
tuliomitico <tulioneedforspeed@gmail.com>
2024-01-16 15:23:41 -0300

    chore: add eslint as dev deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
 import './global.css'
+
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-     <Button>Enviar</Button>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
     </div>
   )
 }
