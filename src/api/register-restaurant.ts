403919289d88a94e41f6417a092b0e11e40be306
tuliomitico <tulioneedforspeed@gmail.com>
2024-02-18 22:33:31 -0300

    feat: add register restaurant api call

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+export interface RegisterRestaurantBody {
+  restaurantName: string
+  managerName: string
+  email: string
+  phone: string
+}
+
+export async function registerRestaurant({
+  restaurantName,
+  managerName,
+  phone,
+  email,
+}: RegisterRestaurantBody) {
+  await api.post('/restaurants', { email, restaurantName, managerName, phone })
+}
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -25,14 +27,21 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<SignUpForm>()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      registerRestaurantFn({
+        email: data.email,
+        managerName: data.managerName,
+        phone: data.phone,
+        restaurantName: data.restaurantName,
+      })
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
     } catch {
