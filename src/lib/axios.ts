3d2b5a8af3727fd776b4eec51da1b7cbcfd89e8a
tuliomitico <tulioneedforspeed@gmail.com>
2024-02-19 11:55:02 -0300

    feat: add skeletons in loading state

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,10 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+api.interceptors.request.use(async(config) => {
+  if (env.VITE_ENABLE_API_DELAY) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+  }
+  return config
+})
