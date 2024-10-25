583a48ca7d634d0eb1f9042ee163ef91ffffc8e4
tuliomitico <tulioneedforspeed@gmail.com>
2024-10-24 21:00:29 -0300

    feat: add real values dashboard cards

--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
+import { getMonthRevenue } from '@/api/get-month-revenue'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function MonthRevenueCard() {
+  const { data: monthRevenue } = useQuery({
+    queryKey: ['metrics', 'month-revenue'],
+    queryFn: getMonthRevenue,
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -12,16 +19,29 @@ export function MonthRevenueCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">
-          {(1248.6).toLocaleString('pt-br', {
-            style: 'currency',
-            currency: 'BRL',
-          })}
-        </span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+2%</span> em
-          relação ao mês passado
-        </p>
+        {monthRevenue && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {(monthRevenue.receipt / 1e6).toLocaleString('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthRevenue?.diffFromLastMonth >= 0 && (
+                <span className="text-emerald-500 dark:text-emerald-400">
+                  +{monthRevenue?.diffFromLastMonth}%
+                </span>
+              )}{' '}
+              {monthRevenue?.diffFromLastMonth < 0 && (
+                <span className="text-red-500 dark:text-red-400">
+                  -{monthRevenue?.diffFromLastMonth}%
+                </span>
+              )}{' '}
+              em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
