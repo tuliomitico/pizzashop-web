7b735fc327f915bea5be2d943d7cd6a6a62392fc
tuliomitico <tulioneedforspeed@gmail.com>
2024-12-29 17:39:05 -0300

    chore: add msw

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  MODE: z.enum(['development', 'test', 'production']),
   VITE_API_URL: z.string().url(),
-  VITE_ENABLE_API_DELAY: z.coerce.boolean().optional()
+  VITE_ENABLE_API_DELAY: z.coerce.boolean().optional(),
 })
 
 export const env = envSchema.parse(import.meta.env)
