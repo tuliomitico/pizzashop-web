959f1a7efccab6a6df2b440a488b9ecbfe72ad84
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-26 16:18:06 -0300

    test: create sign in mock

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   MODE: z.enum(['development', 'test', 'production']),
-  VITE_API_URL: z.string().url(),
+  VITE_API_URL: z.string(),
   VITE_ENABLE_API_DELAY: z.coerce.boolean().optional(),
 })
 
